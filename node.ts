116ab5105cbe3b8aac160fbfda56d5eb7ad49e64
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2022-01-13 23:51:21 +0900

    style: Prettierを再度当てる

--- node.ts
+++ node.ts
@@ -1,7 +1,7 @@
-import { DateTime } from 'luxon';
-import axios from 'axios';
-import { config } from 'dotenv';
-config();
+import axios from 'axios'
+import { DateTime } from 'luxon'
+import { config } from 'dotenv'
+config()
 
 const main = async () => {
   if (
@@ -9,29 +9,32 @@ const main = async () => {
     !process.env.KINTONE_URL ||
     !process.env.KINTONE_APPID
   ) {
-    throw new Error('環境変数が未設定です。');
+    throw new Error('環境変数が未設定です。')
   }
 
-  const now = DateTime.local().setZone('Asia/Tokyo');
+  const now =
+    DateTime.local().setZone('Asia/Tokyo')
   const body = {
     app: process.env.KINTONE_APPID,
     record: {
       text: { value: 'hello!' },
       time: { value: now.toISO() },
     },
-  };
-  console.log(body);
+  }
+  console.log(body)
   const headers = {
-    'X-Cybozu-API-Token': process.env.KINTONE_API_TOKEN,
+    'X-Cybozu-API-Token':
+      process.env.KINTONE_API_TOKEN,
     'Content-Type': 'application/json',
-  };
+  }
 
-  const { data, status, statusText } = await axios.post(
-    `${process.env.KINTONE_URL}/k/v1/record.json`,
-    body,
-    { headers }
-  );
-  console.log({ data, status, statusText });
-};
+  const { data, status, statusText } =
+    await axios.post(
+      `${process.env.KINTONE_URL}/k/v1/record.json`,
+      body,
+      { headers }
+    )
+  console.log({ data, status, statusText })
+}
 
-main();
+main()
