021172508a154eae64446d6a13d6b8cb96d46e29
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2022-01-13 22:55:10 +0900

    feat: TSでkintoneにデータ登録するスクリプト

---
+++ node.ts
@@ -0,0 +1,37 @@
+import { DateTime } from 'luxon';
+import axios from 'axios';
+import { config } from 'dotenv';
+config();
+
+const main = async () => {
+  if (
+    !process.env.KINTONE_API_TOKEN ||
+    !process.env.KINTONE_URL ||
+    !process.env.KINTONE_APPID
+  ) {
+    throw new Error('環境変数が未設定です。');
+  }
+
+  const now = DateTime.local().setZone('Asia/Tokyo');
+  const body = {
+    app: process.env.KINTONE_APPID,
+    record: {
+      text: { value: 'hello!' },
+      time: { value: now.toISO() },
+    },
+  };
+  console.log(body);
+  const headers = {
+    'X-Cybozu-API-Token': process.env.KINTONE_API_TOKEN,
+    'Content-Type': 'application/json',
+  };
+
+  const { data, status, statusText } = await axios.post(
+    `${process.env.KINTONE_URL}/k/v1/record.json`,
+    body,
+    { headers }
+  );
+  console.log({ data, status, statusText });
+};
+
+main();
